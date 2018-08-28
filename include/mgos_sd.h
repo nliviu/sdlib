47fdfc8e06d6025b2e07ca5937f84bb312d99ed8
nliviu <nliviu@gmail.com>
2018-08-28 16:45:28 +0300

    Add unmount SD before reboot
    clang-format

--- include/mgos_sd.h
+++ include/mgos_sd.h
@@ -16,8 +16,7 @@
 #pragma once
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 struct mgos_sd;
@@ -29,23 +28,25 @@ enum mgos_sd_fs_unit {
   SD_FS_UNIT_BYTES = 1,
   SD_FS_UNIT_KILOBYTES = 2,
   SD_FS_UNIT_MEGABYTES = 3,
-  //SD_FS_UNIT_GIGABYTES = 4
+  // SD_FS_UNIT_GIGABYTES = 4
 };
 
 /*
  * Initialize the SD card using the `sdmmc` or `spi` device of ESP32
- * mounts it at `mount_point`, format it if mount failed if `format_if_mount_failed` is true.
+ * mounts it at `mount_point`, format it if mount failed if
+ * `format_if_mount_failed` is true.
  * Returns an opaque pointer if success, NULL otherwise
  * - sdmmc - true -> use `sdmmc`, false -> use `spi
  */
-struct mgos_sd* mgos_sd_open(bool sdmmc, const char* mount_point, bool format_if_mount_failed);
+struct mgos_sd *mgos_sd_open(bool sdmmc, const char *mount_point,
+                             bool format_if_mount_failed);
 
 /*
  * Get the global instance.
  * Valid only after mgos_sd_open.
  * Intended to be used by the rpc service.
  */
-struct mgos_sd* mgos_sd_get_global();
+struct mgos_sd *mgos_sd_get_global();
 
 /*
  * Closes the sd and deletes the `struct mgos_sd*`
@@ -55,30 +56,33 @@ void mgos_sd_close();
 /*
  * Prints information about the connected SD card
  */
-void mgos_sd_print_info(struct json_out* out);
+void mgos_sd_print_info(struct json_out *out);
 
 /*
  * Returns the mount point of the SD card.
  */
-const char* mgos_sd_get_mount_point();
+const char *mgos_sd_get_mount_point();
 
 /*
  * Lists the contents of the SD card.
  */
-bool mgos_sd_list(const char* path, struct json_out* out);
+bool mgos_sd_list(const char *path, struct json_out *out);
 
 /*
- * Returns the size of the SD card using the units defined by `enum mgos_sd_fs_unit`
+ * Returns the size of the SD card using the units defined by `enum
+ * mgos_sd_fs_unit`
  */
 uint64_t mgos_sd_get_fs_size(enum mgos_sd_fs_unit unit);
 
 /*
- * Returns the used size of the SD card using the units defined by `enum mgos_sd_fs_unit`
+ * Returns the used size of the SD card using the units defined by `enum
+ * mgos_sd_fs_unit`
  */
 uint64_t mgos_sd_get_fs_used(enum mgos_sd_fs_unit unit);
 
 /*
- * Returns the free size of the SD card using the units defined by `enum mgos_sd_fs_unit`
+ * Returns the free size of the SD card using the units defined by `enum
+ * mgos_sd_fs_unit`
  */
 uint64_t mgos_sd_get_fs_free(enum mgos_sd_fs_unit unit);
 
--- src/mgos_sd.c
+++ src/mgos_sd.c
@@ -25,74 +25,88 @@
 #include "mgos_sd.h"
 
 struct mgos_sd {
-  sdmmc_card_t* card;
-  char* mount_point;
+  sdmmc_card_t *card;
+  char *mount_point;
   uint64_t size;
 };
 
-static struct mgos_sd* s_card = NULL;
+static struct mgos_sd *s_card = NULL;
 
-static bool get_size_used(uint64_t* total_size, const char* folder);
+static bool get_size_used(uint64_t *total_size, const char *folder);
 
-static struct mgos_sd* mgos_sd_common_init(const char* mount_point,
+static static void unmount_sd_cb(int ev, void *ev_data, void *arg) {
+  (void) ev;
+  (void) ev_data;
+  (void) arg;
+  mgos_sd_close();
+}
+
+static struct mgos_sd *mgos_sd_common_init(const char *mount_point,
                                            bool format_if_mount_failed,
-                                           const sdmmc_host_t* host,
-                                           const void* slot_config) {
+                                           const sdmmc_host_t *host,
+                                           const void *slot_config) {
   // Options for mounting the filesystem.
   // If format_if_mount_failed is set to true, SD card will be partitioned and
   // formatted in case when mounting fails.
   esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-    /**
-     * If FAT partition can not be mounted, and this parameter is true,
-     * create partition table and format the filesystem.
-     */
-    .format_if_mount_failed = format_if_mount_failed,
-    .max_files = 5, ///< Max number of open files
-    /**
-     * If format_if_mount_failed is set, and mount fails, format the card
-     * with given allocation unit size. Must be a power of 2, between sector
-     * size and 128 * sector size.
-     * For SD cards, sector size is always 512 bytes. For wear_levelling,
-     * sector size is determined by CONFIG_WL_SECTOR_SIZE option.
-     *
-     * Using larger allocation unit size will result in higher read/write
-     * performance and higher overhead when storing small files.
-     *
-     * Setting this field to 0 will result in allocation unit set to the
-     * sector size.
-     */
-    .allocation_unit_size = 16 * 1024
-  };
+      /**
+       * If FAT partition can not be mounted, and this parameter is true,
+       * create partition table and format the filesystem.
+       */
+      .format_if_mount_failed = format_if_mount_failed,
+      .max_files = 5,  ///< Max number of open files
+      /**
+       * If format_if_mount_failed is set, and mount fails, format the card
+       * with given allocation unit size. Must be a power of 2, between sector
+       * size and 128 * sector size.
+       * For SD cards, sector size is always 512 bytes. For wear_levelling,
+       * sector size is determined by CONFIG_WL_SECTOR_SIZE option.
+       *
+       * Using larger allocation unit size will result in higher read/write
+       * performance and higher overhead when storing small files.
+       *
+       * Setting this field to 0 will result in allocation unit set to the
+       * sector size.
+       */
+      .allocation_unit_size = 16 * 1024};
 
   // Use settings defined above to initialize SD card and mount FAT filesystem.
   // Note: esp_vfs_fat_sdmmc_mount is an all-in-one convenience function.
   // Please check its source code and implement error recovery when developing
   // production applications.
-  sdmmc_card_t* card = NULL;
-  esp_err_t ret = esp_vfs_fat_sdmmc_mount(mount_point, host, slot_config, &mount_config, &card);
+  sdmmc_card_t *card = NULL;
+  esp_err_t ret = esp_vfs_fat_sdmmc_mount(mount_point, host, slot_config,
+                                          &mount_config, &card);
 
   if (ret != ESP_OK) {
     if (ret == ESP_FAIL) {
       LOG(LL_ERROR, ("Failed to mount filesystem. "
-          "If you want the card to be formatted, set format_if_mount_failed = true."));
+                     "If you want the card to be formatted, set "
+                     "format_if_mount_failed = true."));
     } else {
       const char *err = esp_err_to_name(ret);
       LOG(LL_ERROR, ("Failed to initialize the card (%d - %s). "
-          "Make sure SD card lines have pull-up resistors in place.", ret, err));
+                     "Make sure SD card lines have pull-up resistors in place.",
+                     ret, err));
     }
     return NULL;
   }
 
   // Card has been initialized
-  s_card = (struct mgos_sd*) calloc(1, sizeof (struct mgos_sd));
+  s_card = (struct mgos_sd *) calloc(1, sizeof(struct mgos_sd));
   s_card->card = card;
   s_card->size = ((uint64_t) card->csd.capacity) * card->csd.sector_size;
   s_card->mount_point = strdup(mount_point);
 
+  /*
+   * Add reboot handler to unmount the SD.
+   */
+  mgos_event_add_handler(MGOS_EVENT_REBOOT, unmount_sd_cb, NULL);
+
   return s_card;
 }
 
-static struct mgos_sd* mgos_sd_open_sdmmc(const char* mount_point,
+static struct mgos_sd *mgos_sd_open_sdmmc(const char *mount_point,
                                           bool format_if_mount_failed) {
   LOG(LL_INFO, ("Using SDMMC peripheral"));
 
@@ -100,48 +114,59 @@ static struct mgos_sd* mgos_sd_open_sdmmc(const char* mount_point,
   // To use 1-line SD mode, uncomment the following line:
   // host.flags = SDMMC_HOST_FLAG_1BIT;
 
-  // This initializes the slot without card detect (CD) and write protect (WP) signals.
-  // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+  // This initializes the slot without card detect (CD) and write protect (WP)
+  // signals.
+  // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these
+  // signals.
   sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
 
   // GPIOs 15, 2, 4, 12, 13 should have external 10k pull-ups.
   // Internal pull-ups are not sufficient. However, enabling internal pull-ups
   // does make a difference some boards, so we do that here.
-  mgos_gpio_set_pull(15, MGOS_GPIO_PULL_UP); // CMD, needed in 4- and 1- line modes
-  mgos_gpio_set_pull(2, MGOS_GPIO_PULL_UP); // D0, needed in 4- and 1-line modes
-  mgos_gpio_set_pull(4, MGOS_GPIO_PULL_UP); // D1, needed in 4-line mode only
-  mgos_gpio_set_pull(12, MGOS_GPIO_PULL_UP); // D2, needed in 4-line mode only
-  mgos_gpio_set_pull(13, MGOS_GPIO_PULL_UP); // D3, needed in 4- and 1-line modes
-
-  return mgos_sd_common_init(mount_point, format_if_mount_failed, &host, &slot_config);
-}
-
-static struct mgos_sd* mgos_sd_open_spi(const char* mount_point,
-                                        bool format_if_mount_failed) {
-  LOG(LL_INFO, ("Using SPI peripheral"));
-
-  sdmmc_host_t host = SDSPI_HOST_DEFAULT();
-  sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
-  slot_config.gpio_miso = mgos_sys_config_get_sd_spi_pin_miso(); //PIN_NUM_MISO;
-  slot_config.gpio_mosi = mgos_sys_config_get_sd_spi_pin_mosi(); //PIN_NUM_MOSI;
-  slot_config.gpio_sck = mgos_sys_config_get_sd_spi_pin_clk(); //PIN_NUM_CLK;
-  slot_config.gpio_cs = mgos_sys_config_get_sd_spi_pin_cs(); //PIN_NUM_CS;
-  // This initializes the slot without card detect (CD) and write protect (WP) signals.
-  // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+  mgos_gpio_set_pull(15,
+                     MGOS_GPIO_PULL_UP);  // CMD, needed in 4- and 1- line modes
+  mgos_gpio_set_pull(2,
+                     MGOS_GPIO_PULL_UP);  // D0, needed in 4- and 1-line modes
+  mgos_gpio_set_pull(4, MGOS_GPIO_PULL_UP);   // D1, needed in 4-line mode only
+  mgos_gpio_set_pull(12, MGOS_GPIO_PULL_UP);  // D2, needed in 4-line mode only
+  mgos_gpio_set_pull(13,
+                     MGOS_GPIO_PULL_UP);  // D3, needed in 4- and 1-line modes
 
   return mgos_sd_common_init(mount_point, format_if_mount_failed, &host,
                              &slot_config);
 }
 
-struct mgos_sd* mgos_sd_open(bool sdmmc, const char* mount_point,
+static struct mgos_sd *mgos_sd_open_spi(const char *mount_point,
+                                        bool format_if_mount_failed) {
+  LOG(LL_INFO, ("Using SPI peripheral"));
+
+  sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+  sdspi_slot_config_t slot_config = SDSPI_SLOT_CONFIG_DEFAULT();
+  slot_config.gpio_miso =
+      mgos_sys_config_get_sd_spi_pin_miso();  // PIN_NUM_MISO;
+  slot_config.gpio_mosi =
+      mgos_sys_config_get_sd_spi_pin_mosi();                    // PIN_NUM_MOSI;
+  slot_config.gpio_sck = mgos_sys_config_get_sd_spi_pin_clk();  // PIN_NUM_CLK;
+  slot_config.gpio_cs = mgos_sys_config_get_sd_spi_pin_cs();    // PIN_NUM_CS;
+  // This initializes the slot without card detect (CD) and write protect (WP)
+  // signals.
+  // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these
+  // signals.
+
+  return mgos_sd_common_init(mount_point, format_if_mount_failed, &host,
+                             &slot_config);
+}
+
+struct mgos_sd *mgos_sd_open(bool sdmmc, const char *mount_point,
                              bool format_if_mount_failed) {
   if (NULL != s_card) {
     LOG(LL_ERROR, ("SD already created. Returns the existing instance "
-        "mounted at %s", s_card->mount_point));
+                   "mounted at %s",
+                   s_card->mount_point));
     return s_card;
   }
   return sdmmc ? mgos_sd_open_sdmmc(mount_point, format_if_mount_failed)
-    : mgos_sd_open_spi(mount_point, format_if_mount_failed);
+               : mgos_sd_open_spi(mount_point, format_if_mount_failed);
 }
 
 void mgos_sd_close() {
@@ -156,18 +181,22 @@ void mgos_sd_close() {
   }
 }
 
-struct mgos_sd* mgos_sd_get_global() {
+struct mgos_sd *mgos_sd_get_global() {
   return s_card;
 }
 
-void mgos_sd_print_info(struct json_out * out) {
-  if ((NULL != s_card)&& (NULL != out)) {
-    const sdmmc_card_t* card = s_card->card;
-    json_printf(out, "{Name: %Q, Type: %Q, Speed: %Q, Size: %llu, SizeUnit:%Q, ",
-                card->cid.name, ((card->ocr & SD_OCR_SDHC_CAP) ? "SDHC/SDXC" : "SDSC"),
-                ((card->csd.tr_speed > 25000000) ? "high speed" : "default speed"),
-                (((uint64_t) card->csd.capacity) * card->csd.sector_size / (1024 * 1024)), "MB");
-    json_printf(out, "CSD:{ver:%d, sector_size:%d, capacity:%d, read_bl_len:%d}, ",
+void mgos_sd_print_info(struct json_out *out) {
+  if ((NULL != s_card) && (NULL != out)) {
+    const sdmmc_card_t *card = s_card->card;
+    json_printf(
+        out, "{Name: %Q, Type: %Q, Speed: %Q, Size: %llu, SizeUnit:%Q, ",
+        card->cid.name, ((card->ocr & SD_OCR_SDHC_CAP) ? "SDHC/SDXC" : "SDSC"),
+        ((card->csd.tr_speed > 25000000) ? "high speed" : "default speed"),
+        (((uint64_t) card->csd.capacity) * card->csd.sector_size /
+         (1024 * 1024)),
+        "MB");
+    json_printf(out,
+                "CSD:{ver:%d, sector_size:%d, capacity:%d, read_bl_len:%d}, ",
                 card->csd.csd_ver, card->csd.sector_size, card->csd.capacity,
                 card->csd.read_block_len);
     json_printf(out, "SCR:{sd_spec:%d, bus_width:%d}}", card->scr.sd_spec,
@@ -175,39 +204,40 @@ void mgos_sd_print_info(struct json_out * out) {
   }
 }
 
-const char* mgos_sd_get_mount_point() {
+const char *mgos_sd_get_mount_point() {
   return (NULL != s_card) ? s_card->mount_point : NULL;
 }
 
-bool mgos_sd_list(const char* path, struct json_out * out) {
+bool mgos_sd_list(const char *path, struct json_out *out) {
   if (NULL == s_card) {
     return false;
   }
 
   char buf[256];
-  snprintf(buf, sizeof (buf), "%s/%s", s_card->mount_point, ((NULL == path) ? "" : path));
+  snprintf(buf, sizeof(buf), "%s/%s", s_card->mount_point,
+           ((NULL == path) ? "" : path));
   while ('/' == buf[strlen(buf) - 1]) {
     buf[strlen(buf) - 1] = '\0';
   }
 
-  //check if dir or file
+  // check if dir or file
   bool isDir = false;
   cs_stat_t st;
   if (0 == mg_stat(buf, &st)) {
-    //success
+    // success
     isDir = S_ISDIR(st.st_mode) ? true : false;
     if (!isDir) {
-      json_printf(out, "[{name:%Q, size:%llu, directory:%B}]",
-                  buf, (uint64_t) st.st_size, false);
+      json_printf(out, "[{name:%Q, size:%llu, directory:%B}]", buf,
+                  (uint64_t) st.st_size, false);
       return true;
     }
   } else {
-    //error
+    // error
     LOG(LL_ERROR, ("Could not stat %s", buf));
     return false;
   }
 
-  DIR* dir = opendir(buf);
+  DIR *dir = opendir(buf);
   if (NULL == dir) {
     LOG(LL_ERROR, ("Could not open %s", buf));
     return false;
@@ -219,16 +249,18 @@ bool mgos_sd_list(const char* path, struct json_out * out) {
   char file_path[256];
   struct dirent *entry = readdir(dir);
   while (NULL != entry) {
-    snprintf(file_path, sizeof (file_path), "%s/%s", buf, entry->d_name);
+    snprintf(file_path, sizeof(file_path), "%s/%s", buf, entry->d_name);
     if (0 == mg_stat(file_path, &st)) {
       uint64_t size = st.st_size;
       isDir = S_ISDIR(st.st_mode);
       if (!isDir) {
-        json_printf(out, "%s{name:%Q, size:%llu, directory:%B}", (first ? "" : ", "), entry->d_name, size, isDir);
+        json_printf(out, "%s{name:%Q, size:%llu, directory:%B}",
+                    (first ? "" : ", "), entry->d_name, size, isDir);
       } else {
         uint64_t size = 0;
         get_size_used(&size, file_path);
-        json_printf(out, "%s{name:%Q, size:%llu, directory:%B}", (first ? "" : ", "), entry->d_name, size, isDir);
+        json_printf(out, "%s{name:%Q, size:%llu, directory:%B}",
+                    (first ? "" : ", "), entry->d_name, size, isDir);
       }
       first = false;
       entry = readdir(dir);
@@ -249,8 +281,8 @@ uint64_t mgos_sd_get_fs_size(enum mgos_sd_fs_unit unit) {
   }
   uint64_t size = s_card->size;
   switch (unit) {
-      //case SD_FS_UNIT_GIGABYTES:
-      //    size /= 1024;
+    // case SD_FS_UNIT_GIGABYTES:
+    //    size /= 1024;
     case SD_FS_UNIT_MEGABYTES:
       size /= 1024;
     case SD_FS_UNIT_KILOBYTES:
@@ -261,29 +293,30 @@ uint64_t mgos_sd_get_fs_size(enum mgos_sd_fs_unit unit) {
   return size;
 }
 
-bool get_size_used(uint64_t* total_size, const char* folder) {
+bool get_size_used(uint64_t *total_size, const char *folder) {
   char full_path[256];
   struct stat buffer;
   int exists;
   bool resp = true;
 
-  DIR* dir = opendir(folder);
+  DIR *dir = opendir(folder);
 
   if (dir == NULL) {
     return false;
   }
 
-  struct dirent* dir_data = readdir(dir);
+  struct dirent *dir_data = readdir(dir);
   while (NULL != dir_data) {
     if (dir_data->d_type == DT_DIR) {
       if (dir_data->d_name[0] != '.') {
-        snprintf(full_path, sizeof (full_path), "%s/%s", folder, dir_data->d_name);
+        snprintf(full_path, sizeof(full_path), "%s/%s", folder,
+                 dir_data->d_name);
         if (false == get_size_used(total_size, full_path)) {
           resp = false;
         }
       }
     } else {
-      snprintf(full_path, sizeof (full_path), "%s/%s", folder, dir_data->d_name);
+      snprintf(full_path, sizeof(full_path), "%s/%s", folder, dir_data->d_name);
       exists = stat(full_path, &buffer);
       if (exists < 0) {
         LOG(LL_ERROR, ("stat failed %s (%u)", full_path, errno));
